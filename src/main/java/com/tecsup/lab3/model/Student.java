d71f1fa838efb2a29bd4e87ed8724c95422d9638
YiroVerona <Akazawa>
2021-09-08 01:45:18 -0500

    comentario

--- src/main/java/com/tecsup/lab3/model/Student.java
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -1,10 +1,12 @@
 package com.tecsup.lab3.model;
 
 public class Student {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String estudiante="Yiro Verona";
 	}
 
 }
