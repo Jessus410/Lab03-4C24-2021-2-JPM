d1f5fb33d532c35d11c3fdbb64efb12520beb935
Jesus <marco.portilla@tecsup.edu.pe>
2021-09-08 23:19:35 -0500

    Cambios 2

--- src/main/java/com/tecsup/lab3/controller/StudentController.java
+++ src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -2,6 +2,6 @@ package com.tecsup.lab3.controller;
 
 public class StudentController {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo - Tecsup");
 	}
 }
