e303828e4dd72a5cf8ae9a928412b3841de81af3
Jesus <marco.portilla@tecsup.edu.pe>
2021-09-08 21:46:11 -0500

    Cambios Realizados

--- src/main/java/com/tecsup/lab3/view/StudentReport.java
+++ src/main/java/com/tecsup/lab3/view/StudentReport.java
@@ -1,5 +1,9 @@
 package com.tecsup.lab3.view;
 
 public class StudentReport {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String estudiante="Jesus Portilla";
+	}
 }
